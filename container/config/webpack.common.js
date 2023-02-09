72bc73cdfb4f3fafb3257189e07cfc08f690c0f1
thuongbui <buianhthuong122@gmail.com>
2023-02-09 21:00:40 +0700

    Change webpack loader from babel to swc

--- container/config/webpack.common.js
+++ container/config/webpack.common.js
@@ -3,13 +3,9 @@ module.exports = {
     rules: [
       {
         test: /\.m?js$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-            plugins: ["@babel/plugin-transform-runtime"],
-          },
+          loader: "swc-loader",
         },
       },
     ],
