3d3ab2287139bcfd500241a6b3f078dd5c7f2af5
thuongbui <buianhthuong122@gmail.com>
2023-02-10 21:04:30 +0700

    Set up webpack production for app container

---
+++ container/config/webpack.prod.js
@@ -0,0 +1,27 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
+
+const commonConfig = require("./webpack.common");
+
+const domain = process.env.DOMAIN_APP;
+
+const productionConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: {
+        ...packageJson.dependencies,
+      },
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, productionConfig);
